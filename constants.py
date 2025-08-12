530499ed546c4e3eca4fea56c8a1a68d3950a663
Saidakbar Narzullayev <saidakbarnarzullayev@mail.ru>
2025-08-12 21:39:51 +0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,6 +6,9 @@ ENCODING = "utf-8"
 WORK_DIRPATH = Path(__file__).parent
 LOGS_DIRPATH = WORK_DIRPATH / "logs"
 
+# JSON fayl yo'li o'rniga SQLite ma'lumotlar bazasi fayli yo'lini ko'rsatamiz
+DB_FILEPATH = WORK_DIRPATH / "gifts_data.sqlite"
+
 LOG_FILEPATH = LOGS_DIRPATH / "main.log"
 
 
