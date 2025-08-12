1c91f70ccd28ae42a00a7d29cbb5c9df44725d43
Saidakbar Narzullayev <saidakbarnarzullayev@mail.ru>
2025-08-12 21:40:15 +0500

    Update star_gifts_data.py

--- star_gifts_data.py
+++ star_gifts_data.py
@@ -1,12 +1,7 @@
 from pydantic import BaseModel, Field
-from pathlib import Path
-import simplejson as json
-import constants
 
-class BaseConfigModel(BaseModel, extra="ignore"):
-    pass
-
-class StarGiftData(BaseConfigModel):
+# Bu klass faqat ma'lumotlar tuzilishini aniqlash uchun ishlatiladi
+class StarGiftData(BaseModel, extra="ignore"):
     id: int
     number: int
     sticker_file_id: str
@@ -23,28 +18,3 @@ class StarGiftData(BaseConfigModel):
     last_checked_upgrade_id: int | None = Field(default=None)
     live_topic_id: int | None = Field(default=None)
     gift_slug: str | None = Field(default=None)
-
-class StarGiftsData(BaseConfigModel):
-    DATA_FILEPATH: Path = Field(exclude=True, default=None)
-    notify_chat_id: int | None = Field(default=None)
-    upgrade_live_chat_id: int | None = Field(default=None)
-    star_gifts: list[StarGiftData] = Field(default_factory=list)
-
-    @classmethod
-    def load(cls, data_filepath: Path) -> "StarGiftsData":
-        instance = cls()
-        instance.DATA_FILEPATH = data_filepath
-        try:
-            with data_filepath.open("r", encoding=constants.ENCODING) as file:
-                file_content = file.read()
-                if not file_content:
-                    return instance
-                data = json.loads(file_content)
-                return cls.model_validate({**data, "DATA_FILEPATH": data_filepath})
-        except (FileNotFoundError, json.JSONDecodeError):
-            return instance
-
-    def save(self) -> None:
-        with self.DATA_FILEPATH.open("w", encoding=constants.ENCODING) as file:
-            json.dump(obj=self.model_dump(exclude={"DATA_FILEPATH"}), fp=file, indent=4, ensure_ascii=True, sort_keys=False)
-
